cacadc1c7d10242edef0d393954e45f33cd5e588
John Smith <nurlanmamitov@gmail.com>
2024-03-06 00:51:49 +0500

    dsv

--- asincrone.py
+++ asincrone.py
@@ -1,12 +1,16 @@
 import asyncio
 import json
-from datetime import datetime
 
 
 async def task2(names,prices):
-   with  open("task2.json","w") as file:
-        json.dumps(names)
-        json.dumps(prices)
+   with  open("task2.json","r") as file:
+       data = json.load(file)
+       for i in range(len(names)):
+           dict = {"name":names[i],"price":prices[i]}
+           data["course"].append(dict)
+   with open("task2.json","w") as file:
+       json.dump(data,file,indent=4)
+   print(data)
 
 
 
@@ -17,11 +21,11 @@ async def task1():
         data= json.load(file)
         names =[]
         prices = []
-        for i in data["courses"]:
+        for i in data["course"]:
             names.append(i["name"])
             prices.append(i["price"])
-
-        await asyncio.run(task2(names,prices))
+        print(names,prices,"to task2")
+        return asyncio.gather(task2(names,prices))
 
 
 
@@ -30,6 +34,4 @@ async def main():
     await asyncio.gather(task1())
 
 if __name__ == "__main__":
-    print(datetime.now().time())
     asyncio.run(main())
-    print(datetime.now().time())
